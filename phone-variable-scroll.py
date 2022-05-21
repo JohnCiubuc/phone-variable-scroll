8081dcc4c0250139aa61aff6ef4d6365bbba4156
John Ciubuc <john.ciubuc@ttuhsc.edu>
2022-05-21 13:30:13 -0500

    Scrolling done. Just bug fixes, gui, and sectioning needed

--- phone-variable-scroll.py
+++ phone-variable-scroll.py
@@ -12,12 +12,16 @@ import numpy as np
 import time
 from threading import Thread
 
+
+#0.2 good for images. 0.1 for daily use. Needs some tweaking
+scrollDelay = 0.2
  
 class wheelSender(Thread):
     _wheelDevice = uinput.Device([uinput.REL_WHEEL])
     _delay = -1
-    _delayMasterCounter = 0
+    _delayMasterCounter = -1
     _direction = 0
+    _fingerDown = False
     def __init__(self):
         Thread.__init__(self)
         self.daemon = True
@@ -31,19 +35,26 @@ class wheelSender(Thread):
         if self._delayMasterCounter != delay:
             self._delayMasterCounter = delay
             self._delay = delay
+            # trigger a scroll on new update?
+            self._wheelDevice.emit(uinput.REL_WHEEL, 1) if self._direction >=0 \
+                else self._wheelDevice.emit(uinput.REL_WHEEL, -1)
+    def fingerActive(self, down):
+        self._fingerDown = down
+        print(self._fingerDown)
     def run(self):
         while True:
-            # Fastest sleep speed
-            self._delay = self._delay - 1
-            if self._delay == 0:
-                print(self._direction)
-                self._wheelDevice.emit(uinput.REL_WHEEL, 1) if self._direction >=0 \
-                    else self._wheelDevice.emit(uinput.REL_WHEEL, -1)
-                self._delay = self._delayMasterCounter
-            
-            if self._delay <= -1:
-                self._delay = -1
-            time.sleep(0.01)
+            if self._fingerDown:
+                # Fastest sleep speed
+                self._delay = self._delay - 1
+                # print(self._delay, self._delayMasterCounter)
+                if self._delay == 0:
+                    self._wheelDevice.emit(uinput.REL_WHEEL, 1) if self._direction >=0 \
+                        else self._wheelDevice.emit(uinput.REL_WHEEL, -1)
+                    self._delay = self._delayMasterCounter
+                
+                if self._delay <= -1:
+                    self._delay = -1
+            time.sleep(scrollDelay)
 
 def getScreenSize():
     p1=subprocess.Popen(r'adb shell wm size',shell=True,stdout=subprocess.PIPE)
@@ -63,7 +74,7 @@ def getScreenSize():
 
 
 
-screenDivisions = 24
+screenDivisions = 20
 screenDivisionsHalf = screenDivisions / 2
 
 screen = getScreenSize()
@@ -72,14 +83,23 @@ if screen == None:
     raise SystemExit
 screenSpaces = np.linspace(0,screen[1],screenDivisions)
 wheeler = wheelSender()
+
+
 try:
     p1=subprocess.Popen(r'adb shell getevent',shell=True,stdout=subprocess.PIPE)
     for line in p1.stdout:
         line = line.decode(encoding="utf-8", errors="ignore")
         line = line.strip().split(' ')
+        
+        # BTN_TOOL_FINGER
+        if line[2] == '0145':
+            wheeler.fingerActive({'0': False, '1': True}[line[3][-1]])
+            
+        # ABS_MT_POSITION_Y
         if line[2] == '0036':
             h = int(line[3], 16)
-            index = list(filter(lambda e: e>= h, screenSpaces))[0] #maybe dangerous? but last item shoul be fine
+            #maybe dangerous? but last item shoul be fine
+            index = list(filter(lambda e: e>= h, screenSpaces))[0] 
             index = np.where(screenSpaces==index)
             index = screenDivisionsHalf - index[0][0]
             
