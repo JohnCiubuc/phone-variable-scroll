f5e2d54f7d4295d1927064de0859592a8d853dd8
John Ciubuc <john.ciubuc@ttuhsc.edu>
2022-05-21 00:45:21 -0500

    get screen size

--- phone-variable-scroll.py
+++ phone-variable-scroll.py
@@ -35,5 +35,31 @@ def get_xy():
         
     except Exception as e:
         print(e)
-get_xy()
+# get_xy()
 # print(size)
+
+
+import time
+device = uinput.Device([uinput.REL_WHEEL])
+
+# time.sleep(2);
+ 
+def getScreenSize():
+    p1=subprocess.Popen(r'adb shell wm size',shell=True,stdout=subprocess.PIPE)
+    for line in p1.stdout:
+        line = line.decode(encoding="utf-8", errors="ignore")
+        line = line.strip().split(' ')
+        if len(line) == 3:
+            diff = line[2].split('x')
+            size = ()
+            try:
+                size = (int(diff[0]), int(diff[1]))
+            except Exception as e:
+                print(e)
+            return size
+# for i in range(20):
+#     time.sleep(0.2);
+#     device.emit(uinput.REL_WHEEL, 1)
+# print('done')
+
+print(getScreenSize())
